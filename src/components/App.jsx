22e8b519338371335a8bfc4d826a0eb2b628d51c
amosovandrey <rivercity.dp@gmail.com>
2023-09-30 13:12:53 +0300

    the project is set

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import { useState } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import Section from './Section/Section';
+
+import css from './App.module.css';
+
+function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const onAddContact = newContact => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  const changeFilter = evt => {
+    setFilter(evt.currentTarget.value);
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      <Section title="Phonebook">
+        <ContactForm contacts={contacts} onAddContact={onAddContact} />
+      </Section>
+
+      <Section title="Contacts">
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={deleteContact}
+        />
+      </Section>
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={css.list}>
+    {contacts.map(contact => (
+      <li className={css.list__item} key={contact.id}>
+        <span className={css.list__item__name}>{contact.name}:</span>
+        <span className={css.list__item__number}>{contact.number}</span>
+        <button
+          className={css.list__delete__btn}
+          type="button"
+          onClick={() => onDeleteContact(contact.id)}
+        >
+          x
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
