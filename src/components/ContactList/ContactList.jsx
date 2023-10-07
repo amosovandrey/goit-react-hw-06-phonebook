fa17df0b5ef10c85473c90b0bd108266a8ca0fb9
amosovandrey <rivercity.dp@gmail.com>
2023-10-07 15:45:05 +0300

    styles updated

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,15 +6,18 @@ const ContactList = ({ contacts, onDeleteContact }) => (
   <ul className={css.list}>
     {contacts.map(contact => (
       <li className={css.list__item} key={contact.id}>
-        <span className={css.list__item__name}>{contact.name}:</span>
-        <span className={css.list__item__number}>{contact.number}</span>
-        <button
-          className={css.list__delete__btn}
-          type="button"
-          onClick={() => onDeleteContact(contact.id)}
-        >
-          x
-        </button>
+        <span className={css.list__item__name}>{contact.name}</span>
+        <div className={css.list__item__number__wrapper}>
+          {' '}
+          <span className={css.list__item__number}>{contact.number}</span>
+          <button
+            className={css.list__delete__btn}
+            type="button"
+            onClick={() => onDeleteContact(contact.id)}
+          >
+            x
+          </button>
+        </div>
       </li>
     ))}
   </ul>
